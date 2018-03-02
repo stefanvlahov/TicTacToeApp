74a988d14e61ac60360d99be9f72b11247d30940
stefanvlahov <stefan.vlahov5@gmail.com>
2018-03-02 14:04:06 -0600

    Finalize TicTacToe class. WIP AI class.

---
+++ src/tictactoeapplication/AI.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2017 Stefan Vlahov
+ */
+
+package tictactoeapplication;
+
+/**
+ * @author stefanvlahov
+ */
+
+public class AI {
+
+}
--- src/tictactoeapplication/TicTacToeApplication.java
+++ src/tictactoeapplication/TicTacToeApplication.java
@@ -3,6 +3,8 @@
  */
 package tictactoeapplication;
 
+import java.util.Scanner;
+
 /**
  *
  * @author stefanvlahov
@@ -14,6 +16,18 @@ public class TicTacToeApplication {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        // Allows for cont. games
+        boolean doYouWantToPlay = true;
+        while (doYouWantToPlay) {
+            System.out.println("Welcome To Tic Tac Toe!\n But first, pick the character you want to be and which character the AI will be!");
+            System.out.println();
+            System.out.println("Enter a single character that will represent you on the board!");
+            char playerToken = sc.next().charAt(0);
+            System.out.println("Enter a single character that will represent your opponent on the board");
+            char opponentToken = sc.next().charAt(0);
+        }
+            
     }
     
 }
